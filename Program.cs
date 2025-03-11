8875f473e549a523c5993ed70677214cb1830399
Lab2-31 <Lab2-31@LAB2-31>
2025-03-11 17:47:37 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_SP_WENDY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
---
+++ Videojuegos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_SP_WENDY
+{
+    internal class Videojuegos
+    {
+        public Videojuegos(string nombre, string categoria, int anioDLanzamiento, string personajeFav, string personajePrincipal)
+        {
+            Nombre = nombre;
+            Categoria = categoria;
+            AnioDLanzamiento = anioDLanzamiento;
+            PersonajeFav = personajeFav;
+            PersonajePrincipal = personajePrincipal;
+        }
+
+        public string  Nombre { get; set; }
+        public string Categoria { get; set; }
+        public int AnioDLanzamiento { get; set; }
+        public string PersonajeFav { get; set; }
+        public string PersonajePrincipal { get; set; }
+
+        public Videojuegos () 
+        { }
+
+    }
+}
