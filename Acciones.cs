97f23a7444a08418532f369733ffda41a20d6aeb
Lab2-31 <Lab2-31@LAB2-31>
2025-03-11 18:16:23 -0600

    Examen

--- Acciones.cs
+++ Acciones.cs
@@ -8,5 +8,84 @@ namespace Examen_SP_WENDY
 {
     internal class Acciones
     {
+
+        public List<Videojuegos> ListaVideojuegos = new List<Videojuegos>();
+        Videojuegos a = new Videojuegos();
+
+        public void AgregarVideojuego()
+        {
+            Console.WriteLine("Nombre");
+            a.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Categoria (Terror, Accion, Colaborativo, etc)");
+            a.Categoria = Console.ReadLine();
+
+            Console.WriteLine("Anio de lanzamiento");
+            a.AnioDLanzamiento = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Personaje Favorito");
+            a.PersonajeFav = Console.ReadLine();
+
+            Console.WriteLine("Personaje Principal");
+            a.PersonajePrincipal = Console.ReadLine();
+
+            ListaVideojuegos.Add(new Videojuegos (a.Nombre, a.Categoria, a.AnioDLanzamiento, a.PersonajeFav, a.PersonajePrincipal));
+            Console.WriteLine("Videojuego agregado correctamente");
+
+        }
+
+        public void MostrarVideojuegos()
+        {
+            foreach (var p in ListaVideojuegos)
+            {
+                Console.WriteLine($"Nombre: {p.Nombre}");
+                Console.WriteLine($"Categoria: {p.Categoria}");
+                Console.WriteLine($"Anio de lanzamiento: {p.AnioDLanzamiento}");
+                Console.WriteLine($"Personaje Favorito: {p.PersonajeFav}");
+                Console.WriteLine($"Perosnaje Principal: {p.PersonajePrincipal}");
+            }
+        }
+
+
+        public int ContarVideojuegos()
+        {
+            return ListaVideojuegos.Count;
+        }
+
+        public void EliminarVideojuego(string nombrevideojuego)
+        {
+            var videojuego = ListaVideojuegos.Find(x => x.Nombre == nombrevideojuego);
+            
+            if (videojuego != null)
+            {
+                ListaVideojuegos.Remove(videojuego);
+                Console.WriteLine("Videojuego ELIMINADO");
+            }
+            else
+            {
+                Console.WriteLine("No se encontro el videojuego");
+            }
+        }
+
+        public void ActualizarVideojuego(string nombrevideojuego)
+        {
+            var videojuego = ListaVideojuegos.FirstOrDefault(x => x.Nombre==nombrevideojuego);
+            if ( videojuego != null)
+            {
+                Console.WriteLine("Ingresa los nuevos datos");
+                Console.WriteLine("Nombre");
+
+                Console.WriteLine("Categoria");
+                Console.WriteLine("Anio de lanzamiento");
+                Console.WriteLine("Tu personaje favorito");
+                Console.WriteLine("Personaje Principal");
+            }
+        }
+
+
+
+
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,10 +8,58 @@ namespace Examen_SP_WENDY
 {
     internal class Program
     {
+        public enum Menu
+        {
+            AgregarVJ = 1, MostrarVJ, ContarVJ, EliminarVJ, ActualizarVJ, Salir
+        }
+
         static void Main(string[] args)
         {
+            Acciones acciones = new Acciones();
+
+            while (true)
+            {
+                switch (menu())
+                {
+                    case Menu.AgregarVJ:
+                        acciones.AgregarVideojuego();
+                        break;
+                    case Menu.MostrarVJ:
+                        acciones.MostrarVideojuegos();
+                        break;
+                    case Menu.ContarVJ:
+                        Console.WriteLine(acciones.ContarVideojuegos());
+                        break;
+                    case Menu.EliminarVJ:
+                        Console.WriteLine("Que videojuego quieres borrar");
+                        string nombre = Console.ReadLine();
+                        acciones.EliminarVideojuego(nombre);
+                        break;
+                    case Menu.ActualizarVJ:
+                        Console.WriteLine("Que videojuegas deseas actualizar");
+                        string nombre1 = Console.ReadLine();
+                        acciones.ActualizarVideojuego(nombre1);
+                        break;
+
+                    case Menu.Salir:
+                        Environment.Exit(1);
+                        break;
+
+                }
+            }
             
-            
+        }
+
+        static Menu menu()
+        {
+            Console.WriteLine("1) Agregar videojuego");
+            Console.WriteLine("2) Mostrar videojuegos");
+            Console.WriteLine("3) Contar videojuegos");
+            Console.WriteLine("4) Eliminar videojuego");
+            Console.WriteLine("5) Actualizar videojuego");
+            Console.WriteLine("6) Salir");
+            Menu opc = (Menu) Convert.ToInt32(Console.ReadLine());
+            return opc;
         }
     }
 }
